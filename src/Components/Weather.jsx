48441e1fda754da5d40f8a701246906d584c68b1
Emre Karayaman <karayaman.emre@outlook.com>
2023-02-05 00:54:33 +0300

    style update

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -36,16 +36,19 @@ const Weather = (props) => {
       }
       <div className="weather">
         {weather.main && (
-          <div>
-            <h3>{weather.name}, {weather.sys.country}</h3>
+          <div className='weather-card'>
+            <h2>{weather.name}, {weather.sys.country}</h2>
             <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
-            <p><b>{weather.weather[0].description.charAt(0).toUpperCase() + weather.weather[0].description.slice(1)}</b></p>
-            <p>Temperature: {props.unit === 'C' ? Math.round(weather.main.temp) : Math.round((weather.main.temp * 9) / 5 + 32)}°{props.unit}</p>
-            <p>Minimum Temperature: {props.unit === 'C' ? Math.round(weather.main.temp_min) : Math.round((weather.main.temp_min * 9) / 5 + 32)}°{props.unit}</p>
-            <p>Maximum Temperature: {props.unit === 'C' ? Math.round(weather.main.temp_max) : Math.round((weather.main.temp_max * 9) / 5 + 32)}°{props.unit}</p>
-            <p>Humidity: {weather.main.humidity}%</p>
-            <p>Sunrise: {new Date(weather.sys.sunrise * 1000).toLocaleTimeString()}</p>
-            <p>Sunset: {new Date(weather.sys.sunset * 1000).toLocaleTimeString()}</p>
+            <h3>{weather.weather[0].main}</h3>
+            <h3>{weather.weather[0].description.charAt(0).toUpperCase() + weather.weather[0].description.slice(1)}</h3>
+            <div className='weather-info'>
+              <p>Temperature: {props.unit === 'C' ? Math.round(weather.main.temp) : Math.round((weather.main.temp * 9) / 5 + 32)}°{props.unit}</p>
+              <p>Min: {props.unit === 'C' ? Math.round(weather.main.temp_min) : Math.round((weather.main.temp_min * 9) / 5 + 32)}°{props.unit}</p>
+              <p>Max: {props.unit === 'C' ? Math.round(weather.main.temp_max) : Math.round((weather.main.temp_max * 9) / 5 + 32)}°{props.unit}</p>
+              <p>Humidity: {weather.main.humidity}%</p>
+              <p>Sunrise: {new Date(weather.sys.sunrise * 1000).toLocaleTimeString()}</p>
+              <p>Sunset: {new Date(weather.sys.sunset * 1000).toLocaleTimeString()}</p>
+            </div>
           </div>
         )}
       </div>
