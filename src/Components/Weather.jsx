958a0ca42d9b788d3f629e3d86109d4d2c985d98
Emre Karayaman <karayaman.emre@outlook.com>
2023-02-04 19:27:40 +0300

    update styles

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -24,13 +24,15 @@ const Weather = (props) => {
     <Card className='weather'>
       {weather.main && (
         <CardContent>
-          <Typography variant="h5">{weather.name}</Typography>
+          <Typography variant="h5">{weather.name} , {weather.sys.country}</Typography>
           <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
           <Typography variant="subtitle1">{weather.weather[0].description.charAt(0).toUpperCase() + weather.weather[0].description.slice(1)}</Typography>
           <Typography variant="body2">Temperature: {props.unit === 'C' ? Math.round(weather.main.temp) : Math.round((weather.main.temp * 9) / 5 + 32)}°{props.unit}</Typography>
           <Typography variant="body2">Minimum Temperature: {props.unit === 'C' ? Math.round(weather.main.temp_min) : Math.round((weather.main.temp_min * 9) / 5 + 32)}°{props.unit}</Typography>
           <Typography variant="body2">Maximum Temperature: {props.unit === 'C' ? Math.round(weather.main.temp_max) : Math.round((weather.main.temp_max * 9) / 5 + 32)}°{props.unit}</Typography>
           <Typography variant="body2">Humidity: {weather.main.humidity}%</Typography>
+          <Typography variant="body2">Sunrise: {new Date(weather.sys.sunrise * 1000).toLocaleTimeString()}</Typography>
+          <Typography variant="body2">Sunset: {new Date(weather.sys.sunset * 1000).toLocaleTimeString()}</Typography>
         </CardContent>
       )}
     </Card>
