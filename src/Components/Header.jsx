54f0da58830f0c6d841f7c79118f74b4bbea6722
Emre Karayaman <107217989+karayamanemre@users.noreply.github.com>
2023-02-25 15:44:08 +0300

    style update tailwind

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Header = (props) => {
   return (
-    <header>
-      <form onSubmit={props.handleSubmit}>
-        <input type="text" name="city" placeholder="Search City"/>
-        <input type="submit" value="Search"/>
+    <header class="bg-gray-200 flex p-4 items-center justify-center rounded-full shadow-xl border-2 border-gray-300">
+      <form onSubmit={props.handleSubmit} class="flex items-center justify-center gap-4">
+        <input type="text" name="city" placeholder="Search City" class="px-4 py-2 rounded-full"/>
+        <input type="submit" value="Search" class="bg-orange-600 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded-full cursor-pointer"/>
         <label class="switch">
           <input type="checkbox" onChange={props.handleToggle}/>
           <span class="slider"></span>
--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -23,7 +23,7 @@ const Weather = (props) => {
   }, [props.city]);
 
   return (
-    <main>
+    <main class="container w-full bg-gray-300 flex flex-col items-center justify-center p-6 rounded-xl mt-10 shadow-xl border-2 border-gray-200">
       {loading && 
         <div class="wrapper">
           <div class="circle"></div>
@@ -32,22 +32,25 @@ const Weather = (props) => {
           <div class="shadow"></div>
           <div class="shadow"></div>
           <div class="shadow"></div>
-      </div>
+        </div>
       }
-      <div className="weather">
+      <div class="w-full flex items-center justify-center p-6">
         {weather.main && (
-          <div className='weather-card'>
-            <h2>{weather.name}, {weather.sys.country}</h2>
-            <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
-            <h3>{weather.weather[0].main}</h3>
-            <h3>{weather.weather[0].description.charAt(0).toUpperCase() + weather.weather[0].description.slice(1)}</h3>
-            <div className='weather-info'>
-              <p>Temperature: {props.unit === 'C' ? Math.round(weather.main.temp) : Math.round((weather.main.temp * 9) / 5 + 32)}°{props.unit}</p>
-              <p>Min: {props.unit === 'C' ? Math.round(weather.main.temp_min) : Math.round((weather.main.temp_min * 9) / 5 + 32)}°{props.unit}</p>
-              <p>Max: {props.unit === 'C' ? Math.round(weather.main.temp_max) : Math.round((weather.main.temp_max * 9) / 5 + 32)}°{props.unit}</p>
-              <p>Humidity: {weather.main.humidity}%</p>
-              <p>Sunrise: {new Date(weather.sys.sunrise * 1000).toLocaleTimeString()}</p>
-              <p>Sunset: {new Date(weather.sys.sunset * 1000).toLocaleTimeString()}</p>
+          <div class="card flex flex-col items-center justify-center w-full">
+            <h2 class="text-2xl font-bold">{weather.name}, {weather.sys.country}</h2>
+            <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                alt={weather.weather[0].description}
+                class="w-24 h-24 mx-auto"
+            />
+            <h3 class="text-xl font-bold mb-2">{weather.weather[0].main}</h3>
+            <h3 class="text-lg">{weather.weather[0].description.charAt(0).toUpperCase() + weather.weather[0].description.slice(1)}</h3>
+            <div>
+              <p class="text-gray-800">Temperature: {props.unit === 'C' ? Math.round(weather.main.temp) : Math.round((weather.main.temp * 9) / 5 + 32)}°{props.unit}</p>
+              <p class="text-gray-800">Min: {props.unit === 'C' ? Math.round(weather.main.temp_min) : Math.round((weather.main.temp_min * 9) / 5 + 32)}°{props.unit}</p>
+              <p class="text-gray-800">Max: {props.unit === 'C' ? Math.round(weather.main.temp_max) : Math.round((weather.main.temp_max * 9) / 5 + 32)}°{props.unit}</p>
+              <p class="text-gray-800">Humidity: {weather.main.humidity}%</p>
+              <p class="text-gray-800">Sunrise: {new Date(weather.sys.sunrise * 1000).toLocaleTimeString()}</p>
+              <p class="text-gray-800">Sunset: {new Date(weather.sys.sunset * 1000).toLocaleTimeString()}</p>
             </div>
           </div>
         )}
